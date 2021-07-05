3d44d29459115dcb46cfaf61abbaf40e82d55a48
danielmoessner <github@moesibert.de>
2021-07-05 19:00:49 +0200

    updates

---
+++ get.sh
@@ -0,0 +1,2 @@
+# wget --mirror --page-requisites --adjust-extension --convert-links --backup-converted --no-parent https://kare-bayern.webflow.io/
+wget --mirror --page-requisites --adjust-extension --backup-converted --no-parent https://kare-bayern.webflow.io
